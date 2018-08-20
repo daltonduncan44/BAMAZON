6db1764663832fed225d3347bd5a5e79257e5d1a
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-20 09:24:44 -0400

    database made

---
+++ bamazon_db.sql
@@ -0,0 +1,94 @@
+CREATE DATABASE Bamazon;
+
+USE bamazon;
+
+CREATE TABLE Products (
+ItemID int NOT NULL,
+ProductName varchar(50) NOT NULL,
+DepartmentName varchar(50) NOT NULL,
+Price DECIMAL(4,2) NOT NULL,
+StockQuantity int NOT NULL);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+12345,
+'Pro V1',
+'sports',
+42.00,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+23456,
+'Area rug',
+'home',
+29.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+34567,
+'Matress pad',
+'home',
+49.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+45678,
+'Ipad',
+'electronics',
+299.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+56789,
+'Throw blanket',
+'home',
+29.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+67890,
+'Taylormade m4',
+'sports',
+499.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+78901,
+'Miami Dolphins jersey',
+'clothing',
+99.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+91234,
+'Sun dress',
+'clothing',
+49.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+54321,
+'Winter socks',
+'clothing',
+25.99,
+10);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+65432,
+'Racecar bed',
+'home',
+75.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+45678,
+'Xbox one X',
+'electronics',
+499.99,
+25);
+
+INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
+45678,
+'Beats headphones',
+'electronics',
+199.99,
+25);
